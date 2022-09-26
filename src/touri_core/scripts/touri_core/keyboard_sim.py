2ae7a68872787ce27a96f4124d8946db4188da88
Jashkumar Diyora <jashdiyora7@gmail.com>
2022-09-26 16:54:18 -0400

    Removed keys

--- src/touri_core/scripts/touri_core/keyboard_sim.py
+++ src/touri_core/scripts/touri_core/keyboard_sim.py
@@ -19,8 +19,8 @@ def init_database():
     Init firebase database
     Uses a json key (not in the repo) to form a webSocket connection with the database
     '''
-    cred = credentials.Certificate("/home/hello-robot/Desktop/TouRI/touri_ros/src/touri_core/keys/touri-65f07-firebase-adminsdk-wuv71-3751c21aa8.json")
-    firebase_admin.initialize_app(cred, {'databaseURL': 'https://touri-65f07-default-rtdb.firebaseio.com/'})
+    cred = credentials.Certificate("/") #removed certi
+    firebase_admin.initialize_app(cred, {'databaseURL': '})  #removed url
 
 
 def udpate_db_vals():
